93c0cd3e3df7117412b54d0ba858d85ebb0fa848
wasabina67 <wasabina67@gmail.com>
2024-12-08 16:05:27 +0900

    Update simple_bar_graph.cpp

--- simple_bar_graph.cpp
+++ simple_bar_graph.cpp
@@ -11,8 +11,13 @@ int main() {
     const vector<int> vals = {
         300, 110, 700, 1000
     };
-    const int size = keys.size();
+    const int keys_size = keys.size();
+    const int vals_size = vals.size();
+    if (keys_size != vals_size) return 1;
 
-    cout << size << endl;
+    for (int i = 0; i < keys_size; ++i) {
+        cout << keys[i] << ": " << vals[i] << endl;
+    }
+
     return 0;
 }
